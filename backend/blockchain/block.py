3935faf706671a8755c3da89904780d18c1614cc
Scooter <scott.skrobel@gmail.com>
2021-04-12 14:33:46 -0600

    Added Chain validation and chain replacement.

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -15,8 +15,8 @@ GENESIS_DATA = {
 
 class Block:
     """
-    Block: a unit of storage
-    Store transactions in a blockchain that supports a cryptocurrency 
+    Block: a unit of storage.
+    Store transactions in a blockchain that supports a cryptocurrency.
     """
     def __init__(self, timestamp, last_hash, hash, data, difficulty, nonce):
         self.timestamp = timestamp
@@ -37,15 +37,18 @@ class Block:
             f'nonce: {self.nonce})'
         )
 
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
     @staticmethod
     def mine_block(last_block, data):
         """
-        Mine a block based on the given last_block and data, 
-        until a block hash is found that meets the leading 0's proof of work requirment
+        Mine a block based on the given last_block and data, until a block hash
+        is found that meets the leading 0's proof of work requirement.
         """
         timestamp = time.time_ns()
         last_hash = last_block.hash
-        difficulty = last_block.difficulty
+        difficulty = Block.adjust_difficulty(last_block, timestamp)
         nonce = 0
         hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
@@ -60,20 +63,14 @@ class Block:
     @staticmethod
     def genesis():
         """
-        Generate the genesis block i.e: 1st block in the blockchain.
+        Generate the genesis block.
         """
-        # return Block(
-        #     timestamp=GENESIS_DATA['timestamp'],
-        #     last_hash=GENESIS_DATA['last_hash'],
-        #     hash=GENESIS_DATA['hash'],
-        #     data=GENESIS_DATA['data']
-        # )
         return Block(**GENESIS_DATA)
 
     @staticmethod
     def adjust_difficulty(last_block, new_timestamp):
         """
-        Calculate the adjusted difficulty according to the MINE_RATE
+        Calculate the adjusted difficulty according to the MINE_RATE.
         Increase the difficulty for quickly mined blocks.
         Decrease the difficulty for slowly mined blocks.
         """
@@ -85,11 +82,44 @@ class Block:
 
         return 1
 
+    @staticmethod
+    def is_valid_block(last_block, block):
+        """
+        Validate block by enforcing the following rules:
+          - the block must have the proper last_hash reference
+          - the block must meet the proof of work requirement
+          - the difficulty must only adjust by 1
+          - the block hash must be a valid combination of the block fields
+        """
+        if block.last_hash != last_block.hash:
+            raise Exception('The block last_hash must be correct')
+
+        if hex_to_binary(block.hash)[0:block.difficulty] != '0' * block.difficulty:
+            raise Exception('The proof of work requirement was not met')
+
+        if abs(last_block.difficulty - block.difficulty) > 1:
+            raise Exception('The block difficulty must only adjust by 1')
+
+        reconstructed_hash = crypto_hash(
+            block.timestamp,
+            block.last_hash,
+            block.data,
+            block.nonce,
+            block.difficulty
+        )
+
+        if block.hash != reconstructed_hash:
+            raise Exception('The block hash must be correct')
+
 def main():
     genesis_block = Block.genesis()
-    block = Block.mine_block(genesis_block, 'foo')
-    print(block)
+    bad_block = Block.mine_block(genesis_block, 'foo')
+    bad_block.last_hash = 'evil_data'
 
+    try:
+        Block.is_valid_block(genesis_block, bad_block)
+    except Exception as e:
+        print(f'is_valid_block: {e}')
 
 if __name__ == '__main__':
     main()
--- backend/tests/blockchain/test_block.py
+++ backend/tests/blockchain/test_block.py
@@ -1,5 +1,6 @@
-import time
+
 import pytest
+import time
 
 from backend.blockchain.block import Block, GENESIS_DATA
 from backend.config import MINE_RATE, SECONDS
@@ -19,11 +20,6 @@ def test_genesis():
     genesis = Block.genesis()
 
     assert isinstance(genesis, Block)
-    #assert genesis.timestamp == GENESIS_DATA['timestamp']
-    #assert genesis.last_hash == GENESIS_DATA['last_hash']
-    #assert genesis.hash == GENESIS_DATA['hash']
-    #assert genesis.data == GENESIS_DATA['data']
-
     for key, value in GENESIS_DATA.items():
         getattr(genesis, key) == value
 
@@ -54,3 +50,40 @@ def test_mined_block_difficulty_limits_at_1():
     mined_block = Block.mine_block(last_block, 'bar')
 
     assert mined_block.difficulty == 1
+
+@pytest.fixture
+def last_block():
+	return Block.genesis()
+
+@pytest.fixture
+def block(last_block):
+	return Block.mine_block(last_block, 'test_data')
+
+def test_is_valid_block(last_block, block):
+	Block.is_valid_block(last_block, block)
+
+def test_is_valid_block_bad_last_hash(last_block, block):
+	block.last_hash = 'evil_last_hash'
+
+	with pytest.raises(Exception, match='last_hash must be correct'):
+		Block.is_valid_block(last_block, block)
+
+def test_is_valid_block_bad_proof_of_work(last_block, block):
+	block.hash = 'fff'
+
+	with pytest.raises(Exception, match='proof of work requirement was not met'):
+		Block.is_valid_block(last_block, block)
+
+def test_is_valid_block_jumped_difficulty(last_block, block):
+	jumped_difficulty = 10
+	block.difficulty = jumped_difficulty
+	block.hash = f'{"0" * jumped_difficulty}111abc'
+
+	with pytest.raises(Exception, match='difficulty must only adjust by 1'):
+		Block.is_valid_block(last_block, block)
+
+def test_is_valid_block_bad_block_hash(last_block, block):
+	block.hash = '0000000000000000bbbabc'
+
+	with pytest.raises(Exception, match='block hash must be correct'):
+		Block.is_valid_block(last_block, block)
